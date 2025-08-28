2d97d363984901c6b8431146358974a20c44269f
teknoumut <umut87732@gmail.com>
2025-08-28 14:28:18 +0300

    commit

---
+++ OneDrive/Masaüstü/themes/public/js/script.js
@@ -0,0 +1,19 @@
+document.getElementById("darktema").onclick = () => {
+    document.body.className = "dark-siyah";
+};
+document.getElementById("lighttema").onclick = () => {
+    document.body.classList.remove("darksiyah","dark-mor","darklacivert","bg-sari");
+}
+
+
+document.getElementById("mortema").onclick = () => {
+    document.body.className = "dark-mor";
+};
+
+document.getElementById("laciverttema").onclick = () => {
+    document.body.className = "dark-lacivert";
+};
+document.getElementById("saritema").onclick = () => {
+    document.body.className = "bg-sari";
+};
+
